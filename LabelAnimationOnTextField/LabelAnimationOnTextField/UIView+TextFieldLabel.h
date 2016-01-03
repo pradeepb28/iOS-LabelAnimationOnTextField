ebac508a1a19f4084e32095972898927553f98d0
pradeep <pradeepboorgu@gmail.com>
2016-01-02 21:24:30 -0800

    Category commited

---
+++ LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.h
@@ -0,0 +1,14 @@
+//
+//  UIView+TextFieldLabel.h
+//  LabelAnimationOnTextField
+//
+//  Created by Pradeep Burugu on 1/2/16.
+//  Copyright © 2016 Pradeep Burugu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (TextFieldLabel)
+-(UILabel *)animateLabelTop:(UITextField *)textField;
+
+@end
---
+++ LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.m
@@ -0,0 +1,13 @@
+//
+//  UIView+TextFieldLabel.m
+//  LabelAnimationOnTextField
+//
+//  Created by Pradeep Burugu on 1/2/16.
+//  Copyright © 2016 Pradeep Burugu. All rights reserved.
+//
+
+#import "UIView+TextFieldLabel.h"
+
+@implementation UIView (TextFieldLabel)
+
+@end
