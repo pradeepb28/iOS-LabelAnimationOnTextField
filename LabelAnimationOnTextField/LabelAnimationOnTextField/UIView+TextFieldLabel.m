cfa6412daefb91ec648da5336d3431dd56b3d13c
pradeep <pradeepboorgu@gmail.com>
2016-01-03 00:30:21 -0800

    Added animate label implementation.

--- LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.m
+++ LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.m
@@ -7,24 +7,21 @@
 //
 
 #import "UIView+TextFieldLabel.h"
-#import <objc/runtime.h>
 @implementation UIView (TextFieldLabel)
+
 -(UILabel *)animateLabelTop:(UITextField *)textField{
+//    UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(textField.frame.origin.x, textField.frame.origin.y-10, textField.frame.size.width, 10)];
+//    label.font=[label.font fontWithSize:8];
+//    label.alpha=0.5;
+//    label.text=textField.placeholder;
+    return [self animateLabelTop:textField withFontSize:8];
+}
+-(UILabel *)animateLabelTop:(UITextField *)textField withFontSize:(CGFloat)fontSize{
     UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(textField.frame.origin.x, textField.frame.origin.y-10, textField.frame.size.width, 10)];
-    label.font=[label.font fontWithSize:8];
+    label.font=[label.font fontWithSize:fontSize];
     label.alpha=0.5;
     label.text=textField.placeholder;
     return label;
-
 }
-//- (void)setView:(UIView *)view
-//{
-//    objc_setAssociatedObject(self, (__bridge const void *)(kDHStyleKey), view, OBJC_ASSOCIATION_ASSIGN);
-//
-//}
-//
-//- (NSString*)view
-//{
-//    return objc_getAssociatedObject(self, (__bridge const void *)(kDHStyleKey));
-//}
+
 @end
