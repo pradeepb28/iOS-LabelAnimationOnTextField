2beb39d053f18335e9b052ec2e227c5cceaae7b4
pradeep <pradeepboorgu@gmail.com>
2016-01-03 02:18:54 -0800

    Added Animation

--- LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.m
+++ LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.m
@@ -22,12 +22,19 @@
     return [self animateLabelTop:textField withFontName:fontName withFontSize:8 withColorRed:red andGreen:green andBlue:blue];
 }
 -(UILabel *)animateLabelTop:(UITextField *)textField withFontName:(NSString *)fontName withFontSize:(CGFloat)fontSize withColorRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue{
-    UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(textField.frame.origin.x, textField.frame.origin.y-10, textField.frame.size.width, 10)];
+    UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(textField.frame.origin.x, textField.frame.origin.y, textField.frame.size.width, 10)];
+    label.userInteractionEnabled=NO;
     label.font=[UIFont fontWithName:fontName size:fontSize];
     label.font=[label.font fontWithSize:fontSize];
     label.numberOfLines=1;
+    label.baselineAdjustment=UIBaselineAdjustmentAlignBaselines;
     label.textColor=[UIColor colorWithRed:red green:green blue:blue alpha:0.5];
     label.text=textField.placeholder;
+    [UIView animateWithDuration:0.10 animations:^{
+            label.frame=CGRectMake(textField.frame.origin.x, textField.frame.origin.y-10, textField.frame.size.width, 10);
+    } completion:^(BOOL finished) {
+        label.frame=CGRectMake(textField.frame.origin.x, textField.frame.origin.y-10, textField.frame.size.width, 10);
+    }];
     return label;
 }
 
