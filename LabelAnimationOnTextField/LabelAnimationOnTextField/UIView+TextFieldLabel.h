73ccfe635c4e5a2815490453e501b976ec81532b
pradeep <pradeepboorgu@gmail.com>
2016-01-16 11:53:13 -0800

    additional method

--- LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.h
+++ LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.h
@@ -9,9 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (TextFieldLabel)
-// animates the label to top with default values.
 -(UILabel *)animateLabelTop:(UITextField *)textField;
 -(UILabel *)animateLabelTop:(UITextField *)textField withFontSize:(CGFloat)size;
+-(UILabel *)animateLabelTop:(UITextField *)textField withFontName:(NSString *)fontName;
 -(UILabel *)animateLabelTop:(UITextField *)textField withColorRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue;
 -(UILabel *)animateLabelTop:(UITextField *)textField withFontName:(NSString *)fontName withColorRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue;
 -(UILabel *)animateLabelTop:(UITextField *)textField withFontName:(NSString *)fontName withFontSize:(CGFloat)fontSize withColorRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue;
