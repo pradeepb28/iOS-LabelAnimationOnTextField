115cd3db44fc88905a9cfa775b7f094226da9eef
pradeep <pradeepboorgu@gmail.com>
2016-01-16 11:28:42 -0800

    commit

--- LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.m
+++ LabelAnimationOnTextField/LabelAnimationOnTextField/UIView+TextFieldLabel.m
@@ -7,19 +7,24 @@
 //
 
 #import "UIView+TextFieldLabel.h"
+#define FONT_NAME @"System"
+#define FONT_SIZE 8
+#define RED 0.0
+#define GREEN 0.0
+#define BLUE 0.0
 @implementation UIView (TextFieldLabel)
 
 -(UILabel *)animateLabelTop:(UITextField *)textField{
-    return [self animateLabelTop:textField withFontName:@"System" withFontSize:8 withColorRed:0.0 andGreen:0.0 andBlue:0.0];
+    return [self animateLabelTop:textField withFontName:FONT_NAME withFontSize:FONT_SIZE withColorRed:RED andGreen:GREEN andBlue:BLUE];
 }
 -(UILabel *)animateLabelTop:(UITextField *)textField withFontSize:(CGFloat)size{
-       return [self animateLabelTop:textField withFontName:@"System" withFontSize:size withColorRed:0.0 andGreen:0.0 andBlue:0.0];
+       return [self animateLabelTop:textField withFontName:FONT_NAME withFontSize:size withColorRed:RED andGreen:GREEN andBlue:BLUE];
 }
 -(UILabel *)animateLabelTop:(UITextField *)textField withColorRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue{
-    return [self animateLabelTop:textField withFontName:@"System" withFontSize:8 withColorRed:red andGreen:green andBlue:blue];
+    return [self animateLabelTop:textField withFontName:FONT_NAME withFontSize:FONT_SIZE withColorRed:red andGreen:green andBlue:blue];
 }
 -(UILabel *)animateLabelTop:(UITextField *)textField withFontName:(NSString *)fontName withColorRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue{
-    return [self animateLabelTop:textField withFontName:fontName withFontSize:8 withColorRed:red andGreen:green andBlue:blue];
+    return [self animateLabelTop:textField withFontName:fontName withFontSize:FONT_SIZE withColorRed:red andGreen:green andBlue:blue];
 }
 -(UILabel *)animateLabelTop:(UITextField *)textField withFontName:(NSString *)fontName withFontSize:(CGFloat)fontSize withColorRed:(CGFloat)red andGreen:(CGFloat)green andBlue:(CGFloat)blue{
     UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(textField.frame.origin.x, textField.frame.origin.y, textField.frame.size.width, 10)];
