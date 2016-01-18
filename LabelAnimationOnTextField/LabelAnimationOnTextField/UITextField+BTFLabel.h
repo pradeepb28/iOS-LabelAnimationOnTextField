bad2c37f2a4f5b246e321d74fbe32c33f8a3bd43
pradeep <pradeepboorgu@gmail.com>
2016-01-17 19:37:20 -0600

    category changed

---
+++ LabelAnimationOnTextField/LabelAnimationOnTextField/UITextField+BTFLabel.h
@@ -0,0 +1,13 @@
+//
+//  UITextField+BTFLabel.h
+//  LabelAnimationOnTextField
+//
+//  Created by Pradeep Burugu on 1/17/16.
+//  Copyright © 2016 Pradeep Burugu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (BTFLabel)
+
+@end
